8493c2642937519aa184823e43cabccb92bf558a
Andrei <duircianos@yandex.ru>
2026-04-29 16:54:41 +0400

    somechanges

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -16,7 +16,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://80.64.27.84", "http://80.64.27.84:5173", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		AllowCredentials: true,
